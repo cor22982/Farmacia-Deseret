08d26140b64e73949c82250b604608d91ed5efab
cor22982 <cor22982@uvg.edu.gt>
2024-12-05 00:37:00 -0600

    feat: Complete insert product

--- UI/src/components/ModalForms/ModalProduct.tsx
+++ UI/src/components/ModalForms/ModalProduct.tsx
@@ -1,8 +1,14 @@
-import React, { forwardRef , useState} from 'react';
+import React, { forwardRef , useState, useEffect} from 'react';
 import { Modal, Typography, Box, TextField, Select, MenuItem, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, TextareaAutosize, Button } from '@mui/material';
 import useApi from 'src/hooks/useApi';
 import source_link from 'src/repository/source_repo';
+import useForm from 'src/hooks/useForm';
+import { object, string, number } from 'yup';
+import Swal from "sweetalert2";
+import useToken from 'src/hooks/useToken';
+import { useGetProveedores, Supplier } from 'src/_mock/supplier';
 import { UploadImage } from '../UploadImage/UploadImage';
+
 
 
 interface ModalProductProps {
@@ -21,13 +27,68 @@ const style = {
   boxShadow: 24,
   p: 2,
 };
+
+
+const schema = object({
+  nombre: string().required('El nombre es obligatorio'),
+  forma_f: string().required('La forma farmaceutica es obligatoria'),
+  activo_principal: string().required('El activo principal es obligatorio'),
+  descripcion: string().required('La descripcion es obligatoria')
+})
+
 export const ModalProduct = forwardRef<HTMLDivElement, ModalProductProps>(
   ({ open, handleClose, handleClick }, ref) => {
-    const [value, setValue] = useState('Proveedor');
+
+    const [value_suplier, setValueSupplier] = useState(100000); 
+
+    const {token} = useToken()
+
+    const { getProvedor_ById } = useGetProveedores();
+
+    const [suppliers, setSupliers] = useState<Supplier[]>([]);
+
+    const [value_proveedor, setValue_proveedor] = useState('Proveedor');
+
+
+    const [presentacion, setPresentacion] = useState('ninguno');
+
+    const [isControlado, setControlado] = useState(false);
+
+    const { values: valueForm, setValue: setValueForm, validate, errors } = useForm(schema, { nombre: '', forma_f: '', activo_principal: '', descripcion: ''})
+
     const [image, setImage] = useState<string | null>(null);
-    const {llamadowithFileAndBody, error} = useApi(`${source_link}/upload`)
+
+    const {llamadowithFileAndBody, error} = useApi(`${source_link}/insertProduct`)
     const [file, setFile] = useState<File | null>(null);
 
+    useEffect(() => {
+      const fetchSupplier = async () => {
+        try {
+          const fetchedSuppliers = await getProvedor_ById();
+          setSupliers(fetchedSuppliers)
+          
+        
+        } catch (error_t) {
+          console.error("Error fetching places:", error_t);
+        }
+      };
+  
+      fetchSupplier();
+    }, [getProvedor_ById, setSupliers, suppliers ]);
+    
+    const handleChange_RadioButton = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setControlado((event.target as HTMLInputElement).value === "true");
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+      setValueForm(name as keyof typeof valueForm, value);
+    };
+
+    const handleChange_TextArea = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const { name, value } = e.target;
+      setValueForm(name as keyof typeof valueForm, value);
+    };
     const handleSubmit = async () => {
       if (!file) {
         console.error('No file selected');
@@ -35,18 +96,36 @@ export const ModalProduct = forwardRef<HTMLDivElement, ModalProductProps>(
       }
 
       const formData = {
-        name: 'Example Product',
-        tipo: 'Example Type',
-        activo: 'Example Active Ingredient',
-        descripcion: 'Example Description',
+        token,
+        nombre: valueForm.nombre,
+        forma_f: valueForm.forma_f,
+        presentacion,
+        id_supplier: value_suplier,
+        activo_principal: valueForm.activo_principal,
+        isControlado,
+        descripcion: valueForm.descripcion
       };
 
+      
       const response = await llamadowithFileAndBody(file, formData, 'POST');
       if (response) {
-        console.log('Producto agregado exitosamente', response);
-      } else {
-        console.error('Error al insertar producto', error);
+        if (response.success === true){
+          Swal.fire({
+            icon: "success",
+            title: "Se Inserto de manera exitosa",
+            text: response.message,
+          });
+          handleClick();
+          
+        }else{Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: response.message,
+        });
       }
+        
+      }
+        
     };
 
     return (
@@ -64,9 +143,15 @@ export const ModalProduct = forwardRef<HTMLDivElement, ModalProductProps>(
           <Box display="flex" flexDirection="row" padding="1rem" gap="1rem" width='auto'>
             <TextField
               fullWidth
-              name="name"
+              name="nombre"
               label="Nombre"
               defaultValue=""
+              
+              error={!!errors.nombre}
+              helperText={errors.nombre}
+              onChange={handleChange}
+              value={valueForm.nombre}
+
               InputLabelProps={{ shrink: true }}
               sx={{
                 mb: 0.2,
@@ -86,9 +171,15 @@ export const ModalProduct = forwardRef<HTMLDivElement, ModalProductProps>(
             />
               <TextField
               fullWidth
-              name="tipo"
+              name="forma_f"
               label="Forma Farmaceutica"
               defaultValue=""
+
+              error={!!errors.forma_f}
+              helperText={errors.forma_f}
+              onChange={handleChange}
+              value={valueForm.forma_f}
+              
               InputLabelProps={{ shrink: true }}
               sx={{
                 mb: 0.2,
@@ -128,15 +219,15 @@ export const ModalProduct = forwardRef<HTMLDivElement, ModalProductProps>(
               },
             }}
             
-            value={value}
-            onChange={(e) => {setValue(e.target.value)}}
+            value={presentacion}
+            onChange={(e) => {setPresentacion(e.target.value)}}
           >
-            <MenuItem value="Proveedor">
+            <MenuItem value="ninguno">
               <em>Presentacion</em>
             </MenuItem>
-            <MenuItem value="opcion1">Opción 1</MenuItem>
-            <MenuItem value="opcion2">Opción 2</MenuItem>
-            <MenuItem value="opcion3">Opción 3</MenuItem>
+            <MenuItem value="caja">Caja</MenuItem>
+            <MenuItem value="blister">Blister</MenuItem>
+            <MenuItem value="unidades">Unidades</MenuItem>
           </Select>
           </FormControl>
           <FormControl fullWidth>
@@ -159,15 +250,17 @@ export const ModalProduct = forwardRef<HTMLDivElement, ModalProductProps>(
               },
             }}
             
-            value={value}
-            onChange={(e) => {setValue(e.target.value)}}
+            value={value_suplier}
+            onChange={(e) => setValueSupplier(Number(e.target.value))}
           >
-            <MenuItem value="Proveedor">
-              <em>Proveedores</em>
+            <MenuItem value={100000}>
+              <em>Proveedor</em>
             </MenuItem>
-            <MenuItem value="opcion1">Opción 1</MenuItem>
-            <MenuItem value="opcion2">Opción 2</MenuItem>
-            <MenuItem value="opcion3">Opción 3</MenuItem>
+            {suppliers.map((suplie) => (
+              <MenuItem value={suplie.id}>
+                <em>{suplie.nombre}</em>
+              </MenuItem>
+            ))}
           </Select>
           </FormControl>      
           </Box>
@@ -176,9 +269,15 @@ export const ModalProduct = forwardRef<HTMLDivElement, ModalProductProps>(
           <Box display="flex" flexDirection="row" padding="1rem" gap="1rem" width='auto'>
             <TextField
               fullWidth
-              name="activo"
+              name="activo_principal"
               label="Activo Principal"
               defaultValue=""
+
+              error={!!errors.activo_principal}
+              helperText={errors.activo_principal}
+              onChange={handleChange}
+              value={valueForm.activo_principal}
+
               InputLabelProps={{ shrink: true }}
               sx={{
                 mb: 1,
@@ -203,19 +302,23 @@ export const ModalProduct = forwardRef<HTMLDivElement, ModalProductProps>(
                  row
                  aria-labelledby="demo-row-radio-buttons-group-label"
                  name="row-radio-buttons-group"
-                  
+                 value={isControlado}
+                 onChange={handleChange_RadioButton}
                 >
-                  <FormControlLabel value="si" control={<Radio />} label="Si" />
-                  <FormControlLabel value="no" control={<Radio />} label="No" />
+                  <FormControlLabel value="true" control={<Radio />} label="Si" />
+                  <FormControlLabel value="false" control={<Radio />} label="No" />
                 </RadioGroup>
             </Box>    
           </Box>
           <Box display="flex" flexDirection="row" padding="1rem" gap="1rem" width='auto'>
                
              <TextareaAutosize
-             minRows={4}
-             style={{ width: '100%', borderRadius: '0.5rem' }}
-             placeholder="Descripcion Medicamento"
+              minRows={4}
+              name="descripcion"
+              style={{ width: '100%', borderRadius: '0.5rem' }}
+              placeholder="Descripcion Medicamento"
+              onChange={handleChange_TextArea}
+              value={valueForm.descripcion}
              />
           </Box>
           <UploadImage file={file} setFile={setFile} />
@@ -225,7 +328,7 @@ export const ModalProduct = forwardRef<HTMLDivElement, ModalProductProps>(
             sx={{
               width:'100%'
             }}
-            onClick={handleClick}
+            onClick={handleSubmit}
           >INSERTAR PRODUCTO</Button>
         </Box>
     </Modal>
