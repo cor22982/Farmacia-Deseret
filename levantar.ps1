848a182e00ff265698bbe7747a38347d2a8c226d
cor22982 <cor22982@uvg.edu.gt>
2024-12-23 12:31:26 -0600

    feat: Quit delete for updating own

--- levantar.ps1
+++ levantar.ps1
@@ -4,4 +4,3 @@ Set-Location "E:\Farmacia-Deseret"
 pm2 start ecosystem.config.cjs
 Set-Location "C:\Users\HP\Videos\Farmacia-Deseret\UI"
 pm2 serve dist 4000 --spa
-Read-Host -Prompt "Presiona Enter para salir"
