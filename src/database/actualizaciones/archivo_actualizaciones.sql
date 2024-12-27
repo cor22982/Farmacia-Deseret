d8508fb0b735aa5ae90f5bb3538576a82b7d7dea
cor22982 <cor22982@uvg.edu.gt>
2024-12-26 18:45:31 -0600

    feat: Add actualizaciones

--- src/database/actualizaciones/archivo_actualizaciones.sql
+++ src/database/actualizaciones/archivo_actualizaciones.sql
@@ -1,14 +1,109 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,          -- Identificador único
-    name VARCHAR(50) NOT NULL,      -- Nombre del usuario
-    email VARCHAR(100) UNIQUE,      -- Correo electrónico único
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Fecha de creación
-);
+CREATE OR REPLACE FUNCTION actualizar_products_por_details()
+RETURNS TRIGGER AS $$
+BEGIN
+  DECLARE
+    ganancia_min numeric(10, 4);
+  BEGIN
 
-CREATE TABLE users2 (
-    id SERIAL PRIMARY KEY,          -- Identificador único
-    name VARCHAR(50) NOT NULL,      -- Nombre del usuario
-    email VARCHAR(100) UNIQUE,      -- Correo electrónico único
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Fecha de creación
-);
+    -- Ahora updeteamos la presentacion del producto
+    
+    UPDATE presentacion_producto
+    SET 
+        porcentaje_ganancia =  CASE 
+                                    WHEN pp <> 0 THEN (pp - (cantidad_presentacion*NEW.costo)) / pp
+                                    ELSE 0
+                               END
+    
+    WHERE product_id = NEW.id_product;
+    
 
+    -- Ahora obtenemos la menor ganancia
+
+    SELECT MIN(porcentaje_ganancia)
+    INTO ganancia_min
+    FROM presentacion_producto
+    WHERE product_id = NEW.id_product AND habilitado = true;
+
+    -- Ahora vamos a updetear en el producto
+
+    UPDATE products
+    SET 
+    existencias = existencias + NEW.cantidad,
+    costo = NEW.costo,
+    ganancia = CASE 
+                     WHEN ganancia_min <> 0 THEN ganancia_min
+                     ELSE 0
+                   END
+    WHERE id = NEW.id_product;    
+
+    -- Retorna la nueva fila en productos_cantidades
+    RETURN NEW;
+  END;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Actualizar el Trigger
+DROP TRIGGER IF EXISTS trigger_actualizar_products_por_details ON productos_cantidades;
+
+CREATE OR REPLACE TRIGGER trigger_actualizar_products_por_details
+AFTER INSERT ON productos_cantidades
+FOR EACH ROW
+EXECUTE FUNCTION actualizar_products_por_details();
+
+
+
+
+
+
+
+
+CREATE OR REPLACE FUNCTION actualizar_presentaciones()
+RETURNS TRIGGER AS $$
+BEGIN
+  DECLARE
+    ganancia_min numeric(10, 4);
+    costo_producto numeric(10, 4);
+  BEGIN
+
+    SELECT costo
+    INTO costo_producto
+    FROM products
+    WHERE id = NEW.product_id;
+
+    UPDATE presentacion_producto
+    SET
+        porcentaje_ganancia = CASE 
+                                WHEN  NEW.pp <> 0 and costo_producto IS NOT NULL THEN (NEW.pp - (NEW.cantidad_presentacion*costo_producto)) / NEW.pp
+                                ELSE 0
+                              END
+     WHERE id = NEW.id;
+
+    SELECT MIN(porcentaje_ganancia)
+    INTO ganancia_min
+    FROM presentacion_producto
+    WHERE product_id = NEW.product_id AND habilitado = true;
+
+    UPDATE products
+    SET 
+    ganancia = CASE 
+                     WHEN ganancia_min <> 0 THEN ganancia_min
+                     ELSE 0
+                   END
+    WHERE id = NEW.product_id; 
+
+    RETURN NEW;
+  END;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+-- Actualizar el Trigger
+DROP TRIGGER IF EXISTS trigger_actualizar_presentaciones ON presentacion_producto;
+
+CREATE OR REPLACE TRIGGER trigger_actualizar_presentaciones
+AFTER INSERT ON presentacion_producto
+FOR EACH ROW
+EXECUTE FUNCTION actualizar_presentaciones();
