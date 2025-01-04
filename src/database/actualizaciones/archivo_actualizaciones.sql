36b96bfce16cbad46171caf09ac097d908959a7c
cor22982 <cor22982@uvg.edu.gt>
2025-01-04 03:35:43 -0600

    feaT: Update

--- src/database/actualizaciones/archivo_actualizaciones.sql
+++ src/database/actualizaciones/archivo_actualizaciones.sql
@@ -1,2 +1,11 @@
 alter table products add column dosificacion text;
 alter table products add column accion_farmacologica text;
+
+
+-- Actualizar el Trigger
+DROP TRIGGER IF EXISTS trigger_actualizar_presentaciones ON presentacion_producto;
+
+CREATE OR REPLACE TRIGGER trigger_actualizar_presentaciones
+AFTER INSERT OR UPDATE ON presentacion_producto
+FOR EACH ROW
+EXECUTE FUNCTION actualizar_presentaciones();
