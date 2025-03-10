e385d39e3e837885ab5be2c680c154f8434cd719
cor22982 <cor22982@uvg.edu.gt>
2025-03-10 05:42:57 -0600

    Actualizacion db

--- src/database/actualizaciones/actualizaciones.js
+++ src/database/actualizaciones/actualizaciones.js
@@ -51,7 +51,7 @@ const modifyEnvVariable = async (key, value) => {
 export async function actualizar() {
   try {
     const filePath = 'src\\database\\actualizaciones\\archivo_actualizaciones.sql';
-    const versionNueva = '3';
+    const versionNueva = '4';
 
     // Leer la versión actual directamente desde el archivo .env
     const versionActual = await getEnvVariable('version');
