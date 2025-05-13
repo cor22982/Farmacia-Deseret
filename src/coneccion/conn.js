8d86408b0f27f92535781251b8cfeae7876e7756
Mathew Cordero <97419979+donmatthiuz@users.noreply.github.com>
2025-05-12 20:07:36 -0600

    Update conn.js

--- src/coneccion/conn.js
+++ src/coneccion/conn.js
@@ -6,7 +6,7 @@ import { Sequelize, DataTypes } from 'sequelize';
 dotenv.config({ path: 'src/coneccion/.env' });
 
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USERDB, process.env.PASSWORD, {
   host: 'localhost',
   dialect: 'postgres',
   logging: false,
