381dbdbdf78bfd7dee01476abf756cb3bfb33f13
donmatthiuz <cor22982@uvg.edu.gt>
2025-11-27 00:28:32 -0600

    feat: Agregar todo

--- levantar.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "Levantando proyecto..."
-pm2 delete all
-Set-Location "E:\Farmacia-Deseret"
-pm2 start ecosystem.config.cjs
-Set-Location "C:\Users\HP\Videos\Farmacia-Deseret\UI"
-pm2 serve dist 4000 --spa
