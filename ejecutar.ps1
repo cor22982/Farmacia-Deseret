a0e286db1d7522dab26f44137e369207e28193f5
donmatthiuz <cor22982@uvg.edu.gt>
2026-02-25 03:52:58 -0600

    feat: data

--- ejecutar.ps1
+++ ejecutar.ps1
@@ -2,8 +2,6 @@ Write-Host "Actualizar proyecto..."
 pm2 delete all
 Write-Host "Levantando frontend..."
 cd .\frontend\
-npm install
-npm run build
 pm2 start ecosystem.config.js
 Write-Host "Levantando backend..."
 cd ..
